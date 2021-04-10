ac263de842f3050db1c5dc5d5328bc7a6deacfa5
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-04-10 11:16:59 +0100

    Added ViewModel Factory and ViewModel for the ScoreFragment.kt

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+    private lateinit var viewModel: ScoreViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -46,21 +51,31 @@ class ScoreFragment : Fragment() {
                 false
         )
 
-        // TODO (04) Create and construct a ScoreViewModelFactory
-        // TODO (05) Create ScoreViewModel by using ViewModelProvider as usual, except also
-        // pass in your ScoreViewModelFactory
-
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
 
-        // TODO (07) Convert this class to properly observe and use ScoreViewModel
+        // TODO (04) Create and construct a ScoreViewModelFactory (passing the score) //DONE
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        // TODO (05) Create ScoreViewModel by using ViewModelProvider as usual, except also //DONE
+        // pass in your ScoreViewModelFactory
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+
+
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { shouldPlayAgain ->
+            if(shouldPlayAgain){
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })
+
+        // TODO (07) Convert this class to properly observe and use ScoreViewModel //DONE
+        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         return binding.root
     }
 
-    private fun onPlayAgain() {
-        findNavController().navigate(ScoreFragmentDirections.actionRestart())
-    }
 }
--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -16,7 +16,34 @@
 
 package com.example.android.guesstheword.screens.score
 
-// TODO (01) Create the ScoreViewModel class and have it take in an integer constructor parameter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+// TODO (01) Create the ScoreViewModel class and have it take in an integer constructor parameter //DONE
 // called finalScore
-// TODO (06) Add a LiveData for the score and the play again event, using the best practices for
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+
+    // TODO (06) Add a LiveData for the score and the play again event, using the best practices for
 // encapsulation and event handling that you've learned
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+    get() = _score
+
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain : LiveData<Boolean>
+    get() = _eventPlayAgain
+
+    init {
+        _score.value = finalScore
+    }
+
+    fun onPlayAgain(){
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete(){
+        _eventPlayAgain.value = false
+    }
+}
+
